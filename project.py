e4ec7fdd5a5a6a97cee42b1e41e136c66f9e21d3
EmmetDuggan <73700448+EmmetDuggan@users.noreply.github.com>
2020-11-04 20:25:53 +0000

    Add files via upload
    Added capability to plot data from multiple companies simultaneously. Can search for nearest dates in database if no data for selected dates exists.

--- project.py
+++ project.py
@@ -1,15 +1,97 @@
-from project_io import read_file, connect_to_api
-from project_data_visualisation import time_series
+from project_io import connect_to_api
+from project_calendar import get_valid_dates, find_nearest_date
+from project_data_visualisation import time_series, plot_multiple_time_series
 from project_descriptive_stats import compute_descriptive_stats, make_stats_frame, add_to_frame
+import numpy as np
 
-#print(connect_to_api(input("Enter the service name: >"), "IBM", "demo"))
-data, date_column_name, reverse_data = connect_to_api(input("Enter the service name: >"), "IBM", "NO7SX7BKV0TRLHAM")
-stats = compute_descriptive_stats(data["open"])
-stats_frame = make_stats_frame(stats, "IBM")
+#Function to repeatedly ask for a yes-or-no answer.
+def ask_yes_or_no(question):
+    answer = input(question).lower()
+    valid_answers = ["y", "n"]
+    while True:
+        try:
+            if answer in valid_answers:
+                return answer
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Please enter only \'y\' or \'n\'.")
+            answer = input(question).lower()
 
-data, date_column_name, reverse_data = connect_to_api(input("Enter the service name: >"), "V", "NO7SX7BKV0TRLHAM")
-stats = compute_descriptive_stats(data["open"])
-add_to_frame(stats, "V", stats_frame)
-print(stats_frame)
+#Function to select which analysis user wishes to carry out.
+def print_menu():
+    answer = input("Select one of the following options:\n1. Analysis of Single Company.\n2. Analysis of Multiple Companies.\n")
+    valid_answers = ["1", "2"]
+    while True:
+        try:
+            if answer in valid_answers:
+                return answer
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Please enter only \'1\' or \'2\'.")
+            answer = input("Select one of the following options:\n1. Analysis of Single Company.\n2. Analysis of Multiple Companies.\n")
 
-time_series(data, date_column_name, reverse_data)
+#Function to covert list of tickers seperated for a space into list of
+#capitalised tickers.
+def parse_tickers(ticker_list_in):
+    tickers = ticker_list_in.split(" ")
+    return [ticker.upper() for ticker in tickers]
+
+#Function to add company descriptive statistics to a DataFrame.
+def add_companies_to_frame(service_name, start_date, end_date, frame):
+    tickers = parse_tickers(input("Enter the list of tickers, seperated by a space: >"))
+    data_sets = []
+    for ticker in tickers:
+        data = connect_to_api(service_name, ticker, "NO7SX7BKV0TRLHAM", start_date, end_date)[0]
+        data_sets.append(data)
+        stats = compute_descriptive_stats(data["open"])
+        add_to_frame(stats, ticker, frame)
+    return frame, data_sets
+
+#def make_single_frame(service_name, ticker, start_date = None, end_date = None):
+#Function to create a DataFrame for a single company.
+def make_single_frame(service_name, ticker):
+    start_date, end_date = get_valid_dates()
+    data, date_column_name, reverse_data, new_start, new_end = connect_to_api(service_name, ticker, "NO7SX7BKV0TRLHAM", start_date, end_date)
+    stats = compute_descriptive_stats(data["open"])
+    stats_frame = make_stats_frame(stats, ticker)
+    return stats_frame, data, new_start, new_end
+
+#Function to create a DataFrame for multiple companies.
+def make_full_frame(service_name, start_date = None, end_date = None):
+    #Listed of tickers typed by user is parsed.
+    tickers = parse_tickers(input("Enter the list of tickers, seperated by a space: >"))
+    data_sets = []
+    #An initial DataFrame is created using the first company ticker.
+    frame, data, start_date, end_date = make_single_frame(service_name, tickers[0])
+    #For each ticker in the list, the company statistics are computed and added
+    #to the existing DateFrame.
+    for ticker in tickers:
+        data, date_column_name, reverse_data, new_start, new_end = connect_to_api(service_name, ticker, "NO7SX7BKV0TRLHAM", start_date, end_date)
+        data_sets.append(data)
+        stats = compute_descriptive_stats(data["open"])
+        add_to_frame(stats, ticker, frame)
+    #The DataFrame containing the statistics of all companies is returned along
+    #with the data for the period and tickers.
+    return frame, data_sets, tickers
+
+def main():
+    service_name = input("Enter the service name: >")
+    menu_selection = print_menu()
+    if menu_selection == "1":
+        ticker = input("Enter the company ticker: >")
+        frame, start_date, end_date = make_single_frame(service_name, ticker)
+        time_series(data, ticker, date_column_name, True)
+    if menu_selection == "2":
+        frame, data_sets, tickers = make_full_frame(service_name)
+        plot_multiple_time_series(data_sets, tickers, "timestamp")
+
+    print(frame)
+
+    #time_series(data, ticker, date_column_name, True)
+
+if __name__ == '__main__':
+    main()
--- project_calendar.py
+++ project_calendar.py
@@ -1,12 +1,13 @@
 import datetime
 from datetime import timedelta
 
-
+#Function to convert user input dates to integers.
 def date_in_to_integers(date):
     date_year, date_month, date_day = date.split("-")
     date_year, date_month, date_day = int(date_year), int(date_month), int(date_day)
     return date_year, date_month, date_day
 
+#Function to convert date in integer format to string.
 def date_to_string_hyphen(date_year, date_month, date_day):
     if date_month < 10:
         date_month = "0" + str(date_month)
@@ -14,6 +15,8 @@ def date_to_string_hyphen(date_year, date_month, date_day):
         date_day = "0" + str(date_day)
     return str(date_year) + "-" + str(date_month) + "-" + str(date_day)
 
+#Function to obtain valid dates from the user for which company performance
+#can be analysed.
 def get_valid_dates():
     date_today = datetime.datetime.today()
     start_date_formatted = ""
@@ -22,10 +25,13 @@ def get_valid_dates():
         start_date = input("Enter a start date (yyyy-mm-dd): >")
         end_date = input("Enter an end date (yyyy-mm-dd): >")
 
+        #If dates are not in correct format, a ValueError is raised.
         try:
             s_year, s_month, s_day = start_date.split("-")
             e_year, e_month, e_day = end_date.split("-")
 
+            #Components of dates are converted into integers to check
+            #that end date is preceeded by start date.
             s_year, s_month, s_day = date_in_to_integers(start_date)
             e_year, e_month, e_day = date_in_to_integers(end_date)
 
@@ -40,3 +46,56 @@ def get_valid_dates():
         except ValueError:
             print("The dates entered are invalid. \nEnter dates in the form yyyy-mm-dd. Start date must precede end date.")
     return [start_date_formatted, end_date_formatted]
+
+#Function is get the closest dates to the ones entered via index.
+def get_date_options(dts, dt_differences):
+    minimum_index = dt_differences.index(min(dt_differences))
+    return [datetime.datetime.strftime(dts[minimum_index], '%Y-%m-%d'),
+    datetime.datetime.strftime(dts[minimum_index+1], '%Y-%m-%d')]
+
+#Function to ask the user for which of the available dates they would like
+#to analyse.
+def ask_for_date_selection(unavailable_date, date_options):
+    print("Unfortunately, no data is available for the date", unavailable_date, ". The closest dates are:", date_options)
+    selection = input("Please select one of the dates for which data is available: >")
+    while True:
+        try:
+            if selection in date_options:
+                return selection
+                break
+            else:
+                raise NameError
+        except NameError:
+            print("Enter only one of the available dates in the form yyyy-mm-dd.")
+            selection =input("Please select one of the dates for which data is available: >")
+
+#https://www.dataquest.io/blog/python-datetime-tutorial/
+#Function to find the nearest dates to those entered for which data exists.
+def find_nearest_date(data_dates, start_date, end_date):
+    start_date_dt = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    end_date_dt = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    data_dts = [datetime.datetime.strptime(date, '%Y-%m-%d') for date in data_dates]
+
+    if start_date_dt in data_dts and end_date_dt in data_dts:
+        return start_date, end_date
+
+    elif start_date_dt not in data_dts and end_date_dt in data_dts:
+        dt_differences = [abs(dt - start_date_dt) for dt in data_dts]
+        options = get_date_options(data_dts, dt_differences)
+        new_start = ask_for_date_selection(start_date, options)
+        return new_start, end_date
+
+    elif start_date_dt in data_dts and end_date_dt not in data_dts:
+        dt_differences = [abs(dt - end_date_dt) for dt in data_dts]
+        options = get_date_options(data_dts, dt_differences)
+        new_end = ask_for_date_selection(end_date, options)
+        return start_date, new_end
+
+    if start_date_dt not in data_dts and end_date_dt not in data_dts:
+        start_dt_differences = [abs(dt - start_date_dt) for dt in data_dts]
+        end_dt_differences = [abs(dt - end_date_dt) for dt in data_dts]
+        start_options = get_date_options(data_dts, start_dt_differences)
+        end_options = get_date_options(data_dts, end_dt_differences)
+        new_start = ask_for_date_selection(start_date, start_options)
+        new_end = ask_for_date_selection(end_date, end_options)
+        return new_start, new_end
